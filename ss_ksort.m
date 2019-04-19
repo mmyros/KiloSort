9074074036f6c8db5e175faf583e9f5d5af199ca
max <mmyros@gmail.com>
2019-04-19 11:19:23 -0400

    catch up

--- ss_ksort.m
+++ ss_ksort.m
@@ -1,9 +1,19 @@
 function ss_ksort(respath,nchans,ntrodes)
 % matlab -r -nodisplay -nojvm "ss_ksort('~/res/test2try4/',16,2)"
-% respath='~/res/test2try4/';%put data.dat in htis directory
+%respath='~/res/';%put data.dat in htis directory
 % nchans=16;
 % ntrodes=2;
 %% This part adds paths
+%disp(respath)
+%  disp(nchans)
+%  disp(ntrodes)
+%ntrodes=2;
+%nchans=32;
+try
+ntrodes=str2num(ntrodes);
+nchans=str2num(nchans);
+%f=fopen('~/tempoutput.txt','w');fprintf(f,'%s,%f,%f',respath,nchans,ntrodes);fclose(f) %this was just for test
+end
 addpath(genpath('~/git/ss/mmyksort')) % path to kilosort folder
 addpath(genpath('~/git/npy-matlab')) % path to npy-matlab scripts
 createChannelMapFile(respath,nchans,ntrodes) 
